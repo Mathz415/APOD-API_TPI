5cb576a268e604061335abba28907d300b245bcc
Mathz415 <mattia.montagner2005@gmail.com>
2024-02-01 22:52:45 +0100

    Create Japp.py

---
+++ ES3/Japp.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import requests
+import xmltodict
+import datetime
+
+app = Flask(__name__)
+
+def get_json_data(data):
+    # Costruisci l'URL per ottenere i dati JSON dall'endpoint /json
+    url = f'http://127.0.0.1:5000/json?data={data}'
+    
+    try:
+        # Effettua la richiesta GET all'endpoint /json
+        response = requests.get(url)
+        response.raise_for_status()  # Lancia un'eccezione per errori HTTP
+
+        # Estrai i dati JSON dalla risposta
+        dati_nasa = response.json()
+
+        return dati_nasa
+
+    except requests.exceptions.HTTPError as errh:
+        return {"errore": f"Errore HTTP: {errh}"}
+    except requests.exceptions.ConnectionError as errc:
+        return {"errore": f"Errore di connessione: {errc}"}
+    except requests.exceptions.Timeout as errt:
+        return {"errore": f"Timeout della richiesta: {errt}"}
+    except requests.exceptions.RequestException as err:
+        return {"errore": f"Errore durante la richiesta: {err}"}
+
+@app.route('/', methods=['GET'])
+def visualizza_dati_json():
+    # Ottieni la data selezionata dall'utente o utilizza la data corrente come default
+    data_selezionata = request.args.get('data', str(datetime.date.today()))
+
+    # Chiama la funzione che recupera i dati JSON dall'endpoint /json
+    dati_nasa = get_json_data(data_selezionata)
+
+    return render_template('template.html', dati_nasa=dati_nasa, data_selezionata=data_selezionata)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
